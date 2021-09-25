a5b5e49aa551eced47b83f64220032ba3889b716
ZhuansunYuxiang <183010115@qq.com>
2021-09-25 19:43:28 +0800

    添加将亡值unique_ptr传递测试

--- test.cpp
+++ test.cpp
@@ -1,5 +1,10 @@
 #include "my_tiny_smart_pointer.h"
 #include <iostream>
+auto p_2_int(const int & x)
+{
+    auto tem(make_unique<int>(x));
+    return tem;
+}
 int main()
 {
     //class my_unique_ptr test
@@ -8,5 +13,7 @@ int main()
     std::cout<<*b<<std::endl;
     std::cout<<a.isNull()<<std::endl;
     std::cout<<b.isNull()<<std::endl;
+    auto c = p_2_int(99);
+    std::cout<<*c<<std::endl;
     return 0;
 }
